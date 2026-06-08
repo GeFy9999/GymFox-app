b5e4a132204740934772e260a3c08fe94d6e60b7
GeFy9999 <aatomic702@gmail.com>
2026-06-08 18:11:21 -0400

    style et icons ameliorer

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 export default function Logo() {
   return (
-    <Image
-      src="/gymfoxLogo.webp"
-      alt="Logo GymFox"
-      width={300}
-      height={100}
-      style={{ width: "auto", height: "30px" }}
-      priority
-    />
+    <div className="transition-transform duration-300 hover:scale-105 cursor-pointer">
+      <Image
+        src="/gymfoxLogo.webp"
+        alt="Logo GymFox"
+        width={300}
+        height={100}
+        style={{ width: "auto", height: "30px" }}
+        priority
+      />
+    </div>
   );
 }
